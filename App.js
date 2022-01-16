94717e384d7218aa47b167d09f3ee20e0956121f
Hilful <hilful25@gmail.com>
2022-01-17 01:42:31 +0600

    final commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link, NativeRouter, Route, Routes } from 'react-router-native';
+import Doctors from './Components/Doctors';
+import Home from './Components/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello From React Native!</Text>
+      <Text>Testing it on another line and it is fun</Text>
       <StatusBar style="auto" />
+      <NativeRouter>
+        <Link to="/"><Text>Home</Text></Link>
+        
+        <Link to="/home"><Text>Home</Text></Link>
+        <Link to="/doctors"><Text>Doctors</Text></Link>
+        <Routes>
+          <Route path="/" element={<Home></Home>} ></Route>
+          <Route path="/home" element={<Home></Home>} ></Route>
+          <Route path="/doctors" element={<Doctors></Doctors>} ></Route>
+
+        </Routes>
+      </NativeRouter>
     </View>
   );
 }
